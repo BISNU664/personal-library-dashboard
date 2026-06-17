68c6daf5713a75f08b78dbad906bef9942f5b356
BISNU644 <blim880@gmail.com>
2026-06-17 21:28:07 +1000

    Added books API endpoint and display books in frontend

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,46 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:5174",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+books = [
+    {
+        "id": 1,
+        "title": "Crime and Punishment",
+        "author": "Fyodor Dostoevsky",
+        "status": "Completed",
+    },
+    {
+        "id": 2,
+        "title": "Red Rising",
+        "author": "Pierce Brown",
+        "status": "Reading",
+    },
+    {
+        "id": 3,
+        "title": "American Psycho",
+        "author": "Bret Easton Ellis",
+        "status": "To Read",
+    },
+]
 
 
 @app.get("/")
 def root():
     return {"message": "Personal Library API is running"}
+
+
+@app.get("/books")
+def get_books():
+    return books
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
+
+interface Book {
+  id: number;
+  title: string;
+  author: string;
+  status: string;
+}
+
 function App() {
+  const [books, setBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/books")
+      .then((response) => response.json())
+      .then((data) => setBooks(data));
+  }, []);
+
   return (
     <div>
       <h1>📚 Personal Library Dashboard</h1>
-      <p>Track your reading journey.</p>
+
+      {books.map((book) => (
+        <div key={book.id}>
+          <h2>{book.title}</h2>
+          <p>Author: {book.author}</p>
+          <p>Status: {book.status}</p>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 }
